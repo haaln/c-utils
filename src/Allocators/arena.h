c8e76469fb14a4e7569678d916f9690164c4e23a
haaln <hassan@alnoori.group>
2026-01-07 10:01:57 +0100

    fix:size alignment fix

--- src/Allocators/arena.h
+++ src/Allocators/arena.h
@@ -8,20 +8,25 @@
 #endif
 
 typedef struct arena_t {
-    char *buffer = nullptr;
-    uint64_t capacity = 0;
-    uint64_t offset = 0;
-    uint64_t prev_offset = 0;
-    uint64_t alignment = 0;
+  char *buffer = nullptr;
+  uint64_t capacity = 0;
+  uint64_t offset = 0;
+  uint64_t prev_offset = 0;
+  uint64_t alignment = 0;
 } arena_t;
 
-void *arena_alloc(arena_t *arena, uint64_t size, uint64_t align = (2 * sizeof(void *)));
-arena_t *arena_init(uint64_t size, arena_t *arena = nullptr, char *user_buffer = nullptr, uint64_t align = 2);
+void *arena_alloc(arena_t *arena, uint64_t size,
+                  uint64_t align = (sizeof(void *)));
+arena_t *arena_init(uint64_t size, arena_t *arena = nullptr,
+                    char *user_buffer = nullptr,
+                    uint64_t align = (sizeof(void *)));
 void arena_clear(arena_t *arena);
 void arena_destroy(arena_t *arena);
 
-#define ArenaPush(arena, size)             arena_alloc(arena, size, 2 * sizeof(void *))
-#define ArenaPushStruct(arena, type)       (type *)arena_alloc(arena, sizeof(type), alignof(type))
-#define ArenaPushArray(arena, count, type) arena_alloc(arena, (count) * sizeof(type), alignof(type[1]))
+#define ArenaPush(arena, size) arena_alloc(arena, size, sizeof(void *))
+#define ArenaPushStruct(arena, type)                                           \
+  (type *)arena_alloc(arena, sizeof(type), alignof(type))
+#define ArenaPushArray(arena, count, type)                                     \
+  arena_alloc(arena, (count) * sizeof(type), alignof(type[1]))
 
 #endif // UTIL_ARENA_H
